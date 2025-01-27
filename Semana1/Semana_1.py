99bd9c10f580c02178129e9cd5b093ff47cad66f
Nahomi-25 <nube4674@gmail.com>
2025-01-26 21:41:33 -0600

    Create_Programa1

---
+++ Semana1/Semana_1.py
@@ -0,0 +1,53 @@
+# Ejercicio 1 
+¿Cuál es la función de la memoria secundaria en una computadora?
+c= Almacenar información durante mucho tiempo, incluso después de ciclos de apagado
+y encendido.
+
+#Ejercicio 2
+¿Qué es un programa?
+Son una serie de instrucciones escritas en un código de programación que la computadora puede ejecutar para solucionar un problema.
+
+# Ejercicio 3
+¿Cuál es la diferencia entre un compilador y un intérprete?
+El compilador necesita un código completo para poder traducirlo mientras que un intérprete es más intercativo, ya que puedes crear y modificar el código y el programa puede dar retroalimentación del código.
+
+# Ejercicio 4
+¿Cuál de los siguientes contiene “código máquina”?
+a) El intérprete de Python.
+
+# Ejercicio 5
+¿Qué está mal en el siguiente código?:
+>>> primt ’¡Hola, mundo!’
+File "<stdin>", line 1
+primt ’¡Hola, mundo!’
+^
+SyntaxError: invalid syntax
+>>>
+
+La palabra print que esta mal escrita, después debería de estar el parentesis que contenga el mensaje a mostrar entre comillas.
+
+# Ejercicio 6
+¿En qué lugar del computador queda almacenada una variable, como en este caso “x”,
+después de ejecutar la siguiente línea de Python?:
+x = 123
+b) Memoria Principal.
+
+#Ejercicio 7
+¿Qué mostrará en pantalla el siguiente programa?:
+x = 43
+x = x + 1
+print(x)
+b) 44.
+
+# Ejercicio 8
+Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad
+humana:
+1. Unidad central de procesamiento: la capcidad de aprender
+2. Memoria principal: la memoria a corto plazo
+3. Memoria secundaria: los recuerdos
+4. Dispositivos de entrada: escuchar
+5. Dispositivos de salida: las formas de expresión como hablar.
+
+#Ejercicio 9
+¿Cómo puedes corregir un “Error de sintaxis”?
+Revisando la "retroalimentación" que da el sistema y principalmente comprobando si se siguieron las reglas del programa (símbolos y palabras reservadas)
